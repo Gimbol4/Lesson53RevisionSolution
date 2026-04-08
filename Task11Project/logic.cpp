1434e9d8a6e1c25a0ea77f6cdbc2ae4e1c2ff0b9
Gimbol4 <Shishkanov.yd@gmail.com>
2026-04-08 18:13:44 +0300

    solve 11 task

--- Task11Project/logic.cpp
+++ Task11Project/logic.cpp
@@ -3,3 +3,15 @@
 //
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет среднее арифметическое элементов вектора.
+#include "logic.h"
+
+double recursion(int vector[], int size) {
+    if (size <= 0) {
+        return 0;
+    }
+    return vector[size - 1] + recursion(vector, size - 1);
+}
+double calculate_average(int vector[], int size) {
+    if (size <= 0) return -1;
+    return recursion(vector, size) / size;
+}
