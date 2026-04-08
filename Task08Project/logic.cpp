82a8cd05d63cdd20e7c5b69ede95215c4a6275bc
Gimbol4 <Shishkanov.yd@gmail.com>
2026-04-08 17:20:28 +0300

    solve 8 task

--- Task08Project/logic.cpp
+++ Task08Project/logic.cpp
@@ -4,3 +4,19 @@
 // Необходимо спроектировать и реализовать программу, 
 // которая находит n-ное число в последовательности 
 // степеней двойки: 2, 4, 8, 16, 32, ...
+#include "logic.h"
+
+int recursion(int index) {
+	if (index == 1) {
+		return 2;
+	}
+
+	return 2 * recursion(index - 1);
+}
+
+int get_number(int index) {
+	if (index < 1) {
+		return -1;
+	}
+	return recursion(index);
+}
