1c2db48e15fda79b6c292150cc11754d29c047e9
Gimbol4 <Shishkanov.yd@gmail.com>
2026-04-08 17:05:16 +0300

    solve 6 task

--- Task06Project/logic.cpp
+++ Task06Project/logic.cpp
@@ -3,3 +3,25 @@
 //
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет сумму чётных чисел от 1 до N.
+
+#include "logic.h"
+
+int recursion(int number) {
+	if (number < 2)
+		return 0;
+	if (number % 2 == 0) {
+		return number + recursion(number - 2);
+	}
+
+	else {
+		return recursion(number - 1);
+	}
+}
+
+int sum(int number) {
+	if (number < 0) {
+		return -1;
+	}
+	return recursion(number);
+}
+
