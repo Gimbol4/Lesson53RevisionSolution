6b2ed86a4ff2b804b63d5f3d6eca60fa06dd6c37
Gimbol4 <Shishkanov.yd@gmail.com>
2026-04-08 18:22:59 +0300

    solve 12 task

--- Task12Project/logic.cpp
+++ Task12Project/logic.cpp
@@ -5,3 +5,18 @@
 // для вычисления n-го числа в последовательности, 
 // где каждое следующее число равно сумме предыдущего 
 // и его номера: 1, 2, 4, 7, 11, 16, ...
+#include "logic.h"
+
+int recursion(int index) {
+	if (index <= 1) {
+		return 1;
+	}
+	return recursion(index - 1) + (index - 1);
+}
+
+int get_number(int index) {
+	if (index < 1) {
+		return -1;
+	}
+	return recursion(index);
+}
