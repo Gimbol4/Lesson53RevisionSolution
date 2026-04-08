7ab1c415e6f994fd200f807043a902a7f6a6c4e4
Gimbol4 <Shishkanov.yd@gmail.com>
2026-04-08 17:27:32 +0300

    solve 9 task

--- Task09Project/logic.cpp
+++ Task09Project/logic.cpp
@@ -3,3 +3,24 @@
 //
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет сумму цифр заданного числа, кратных 5
+#include "logic.h"
+
+int recursion(int number) {
+	if (number == 0) {
+		return 0;
+	}
+    int last_digit = number % 10;
+    int current_sum = 0;
+
+    if (last_digit % 5 == 0 && last_digit != 0) {
+        current_sum = last_digit;
+    }
+    return current_sum + recursion(number / 10);
+}
+
+int sum(int number) {
+    if (number < 0) {
+        number = abs(number);
+    }
+    return recursion(number);
+}
